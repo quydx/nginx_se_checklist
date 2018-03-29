cfe29eeb42d2875db0cdbc92e9c29a931f4fe287
quydx <quydx@users.noreply.github.com>
2018-03-29 10:21:20 +0700

    update

--- nginx_checklist.py
+++ nginx_checklist.py
@@ -249,7 +249,9 @@ def audit6a():
     state = PASSED
     cmd = 'find /etc/nginx -type d -printf "%m:%f\n"'
     out = __salt__['cmd.run'](cmd).splitlines()
-    permissions = list(set(out[0].split(":")))
+    permissions = []
+    for line in out:
+        permissions.append(line.split(":")[0])
     if not len(permissions) == 1:
         configs.append("folder permission has value difference to 755")
         configs.append(out)
